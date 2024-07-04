f3e9ef2a759e672cbe30f36d58726c5b573ebe69
Adam Kowalski <adam.kowalski.work@gmail.com>
2024-07-03 21:21:51 -0700

    send textobjects

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -23,11 +23,51 @@ return {
       },
       ignore_blank_lines = true
     })
+
+    _G.iron_send_operator = function(motion_type)
+      local saved_register = vim.fn.getreg('"')
+      local saved_register_type = vim.fn.getregtype('"')
+
+      if motion_type == 'line' then
+        vim.cmd('normal! `[V`]y')
+      elseif motion_type == 'char' then
+        vim.cmd('normal! `[v`]y')
+      else
+        return
+      end
+
+      local yanked_text = vim.fn.getreg('"')
+      iron.send('julia', yanked_text)
+      vim.fn.setreg('"', saved_register, saved_register_type)
+    end
+
+    local function setup_send_operator()
+      vim.o.operatorfunc = 'v:lua.iron_send_operator'
+      return 'g@'
+    end
+
     require("which-key").register({
-      s = { iron.send_line, "send" },
-    }, { mode = "n" })
+      S = { 
+        function()
+          iron.send_line()
+        end, 
+        "send line" 
+      },
+      s = { 
+        function() 
+          return setup_send_operator()
+        end, 
+        "send operator" 
+      },
+    }, { mode = "n", expr = true })
+
     require("which-key").register({
-      s = { iron.visual_send, "send" },
+      s = { 
+        function()
+          iron.visual_send()
+        end, 
+        "send" 
+      },
     }, { mode = "v" })
   end
 }
